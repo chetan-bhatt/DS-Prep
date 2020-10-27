4afd0e8d0275a2213af16fbe31db67fe394efd35
Chetan Bhatt <bhatt.chetan@hotmail.com>
2020-10-27 06:46:59 -0700

    corrected

--- src/dp/MinCoinChange.java
+++ src/dp/MinCoinChange.java
@@ -1,54 +1,37 @@
 package dp;
 
 public class MinCoinChange {
-	
-	static int[] minArr;
-	static int minCoinChange(int[] coins, int amount) {
-		return minCoinChangeHelper(coins, amount);
-	//	minArr = new int[amount + 1];
-	//	return minCoinChangeTopDownHelper(coins, amount, minArr);
+
+	public int coinChange(int[] coins, int amount) {
+		int[] dp = new int[amount + 1];
+
+		return helper(coins, amount, dp);
+
 	}
 
-	static int minCoinChangeHelper(int[] coins, int amount) {
-		if(amount < 0) {
-			return 0;
-		}
-		if(amount == 0) {
-			return 1;
-		}
-		
-		int min = Integer.MAX_VALUE;
-		for(int c : coins) {
-			min  = Math.min(min, 1 + minCoinChangeHelper(coins, amount-c));
-		}
-		return min;
-	}
-	
-	static int minCoinChangeTopDownHelper(int[] coins, int remainder, int[] dp ) {
-		if(remainder < 0) {
+	private int helper(int[] coins, int remainder, int[] dp) {
+		if (remainder < 0) {
 			return -1;
 		}
-		if(remainder == 0) {
+		if (remainder == 0) {
 			return 0;
 		}
-		if(dp[remainder] != 0) {
+		if (dp[remainder] != 0) {
 			return dp[remainder];
 		}
 		int min = Integer.MAX_VALUE;
-		for(int c : coins) {
-			int changeResult = minCoinChangeTopDownHelper(coins, remainder-c, dp);
-			if(changeResult >= 0 && changeResult < min) {
-				min = 1 + changeResult;
+		for (int i = 0; i < coins.length; i++) {
+			int val = helper(coins, remainder - coins[i], dp);
+			if (val >= 0 && val < min) {
+				min = val + 1;
 			}
+			dp[remainder] = min == Integer.MAX_VALUE ? -1 : min;
 		}
-		dp[remainder] = min;
-		
 		return dp[remainder];
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.println(minCoinChange(new int[]{1, 2, 3, 4}, 6));
-		System.out.println(minCoinChange(new int[]{1, 2, 3, 4}, 6));
+		System.out.println(new MinCoinChange().coinChange(new int[] { 1, 2, 3, 4 }, 6));
 
 	}
 }
