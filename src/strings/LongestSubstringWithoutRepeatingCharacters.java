dd7aa69704a39f23bfd2bc1018136d0af99c8277
chetan-bhatt <bhatt.chetan@hotmail.com>
2021-01-21 01:07:29 +0530

    Update LongestSubstringWithoutRepeatingCharacters.java

--- src/strings/LongestSubstringWithoutRepeatingCharacters.java
+++ src/strings/LongestSubstringWithoutRepeatingCharacters.java
@@ -5,31 +5,25 @@ import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
-	
 	public int lengthOfLongestSubstring(String s) {
-        if(s==null||s.length()==0){
-        		return 0;
-        }
-        
-        int result = 1;
-        int j = 0;
-        Set<Character> set = new HashSet<>();
-        
-        for(int i = 0; i < s.length(); i++) {
-        		char c = s.charAt(i);
-        		if(!set.contains(c)) {
-        			set.add(c);
-        			result = Math.max(result,  set.size());
-        		} else {
-        			 while(j < i && s.charAt(j) != c){
-                         set.remove(s.charAt(j++));
-                 }
-                 j++;
-        		}
-        }
-        
-        return result;
-	}
+		if(s == null || s.length() == 0){
+		    return 0;
+		}
+
+		Map<Character, Integer> map = new HashMap<>();
+		int i = 0, j = 0, longest = 0;
+
+		while(j < s.length()){
+		    if(map.containsKey(s.charAt(j))){
+			i = Math.max(i, map.get(s.charAt(j)));
+		    }
+		    map.put(s.charAt(j), j + 1);
+		    longest = Math.max(longest, j - i + 1);
+		    j++;
+		}
+		return longest;
+    	}
+	
 	
 	public static void main(String[] args) {
 		LongestSubstringWithoutRepeatingCharacters obj = new LongestSubstringWithoutRepeatingCharacters();
